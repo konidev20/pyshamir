d901f99b72ca676233bbe8a9a51f008995193456
Hitesh.Balegar <hitesh.balegar@betsol.com>
2022-11-12 18:38:06 +0530

    code cleanup

--- pyshamir/shamir.py
+++ pyshamir/shamir.py
@@ -1,6 +1,33 @@
-from ._utils import add, mul, div, interpolate_polynomial
+import secrets
 
-def combine(parts):
+from ._utils import add, mul, div , make_polynomial
+
+
+def interpolate_polynomial(x_samples: bytearray, y_samples: bytearray, x: int):
+    """
+    Takes N sample points and returns the value of the polynomial at x using Lagrange interpolation
+    """
+    limit = len(x_samples)
+    result = 0
+    for i in range(limit):
+        basis = 1
+        for j in range(limit):
+            if i != j:
+                num = add(x, x_samples[j])
+                den = add(x_samples[i], x_samples[j])
+                term = div(num, den)
+                basis = mul(basis, term)
+        group = mul(y_samples[i], basis)
+        result = add(result, group)
+    return result
+
+
+def combine(parts: list[bytearray]) -> bytearray:
+    """
+    Takes a list of parts and returns the secret
+    :param parts:
+    :return:
+    """
     # Verify enough parts are present
     if len(parts) < 2:
         raise Exception("Not enough parts to combine")
@@ -26,7 +53,7 @@ def combine(parts):
     check_map = {}
 
     for i, part in enumerate(parts):
-        samp = part[first_part_len-1]
+        samp = part[first_part_len - 1]
         if samp in check_map:
             raise Exception("Duplicate sample")
         check_map[samp] = True
@@ -44,3 +71,51 @@ def combine(parts):
         secret[idx] = val
 
     return secret
+
+
+def split(secret: bytes, parts: int, threshold: int) -> list[bytearray]:
+    """
+    Takes a secret and splits it into parts
+    :param secret:
+    :param parts:
+    :param threshold:
+    :return:
+    """
+    # Sanity check the input
+    if parts < threshold:
+        raise Exception("Parts must be greater than threshold")
+    if parts > 255:
+        raise Exception("Parts must be less than 256")
+    if threshold < 2:
+        raise Exception("Threshold must be greater than 1")
+    if threshold > 255:
+        raise Exception("Threshold must be less than 256")
+    if len(secret) < 1:
+        raise Exception("Secret must be at least 1 byte long")
+
+    # Generate random list of x coordinates
+    x_coordinates = [secrets.randbelow(255) for _ in range(1, 256)]
+
+    # Allocate the output array , initalize the final byte of the output with the offset.
+    # This is used to ensure that the same secret can be split into different parts
+    output = [bytearray() for _ in range(parts)]
+    for i in range(len(output)):
+        output[i] = bytearray(len(secret) + 1)
+        output[i][len(secret)] = int(x_coordinates[i]) + 1
+
+    for i, val in enumerate(secret):
+        polynomial_instance = make_polynomial(val, int(threshold - 1))
+
+        for j in range(parts):
+            x = int(x_coordinates[j]) + 1
+            y = polynomial_instance.evaluate(x)
+            output[j][i] = y
+
+    return output
+
+
+
+
+
+
+
