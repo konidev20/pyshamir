367097e19a72437210266f5c33fa3297a8185167
Srigovind Nayak <sgovind.dev@outlook.com>
2022-11-12 10:37:38 +0000

    chore: moved interpolate_polynomial to util

--- pyshamir/_utils.py
+++ pyshamir/_utils.py
@@ -1,9 +1,7 @@
 from ._constants import LOG_TABLE, EXP_TABLE
 
-
 def add(a, b):
     return a ^ b
-
 
 def mul(a, b):
     """
@@ -27,3 +25,21 @@ def div(a, b):
     diff = ((int(log_a) - int(log_b)) + 255) % 255
     ret = int(EXP_TABLE[diff])
     return int(ret)
+
+def interpolate_polynomial(x_samples, y_samples, x):
+    """
+    Takes N sample points and returns the value of the polynomial at x using Lagrange interpolation
+    """
+    limit = len(x_samples)
+    result = 0
+    for i in range(limit):
+        basis = 1
+        for j in range(limit):
+            if i != j:
+                num = add(x, x_samples[j])
+                den = add(x_samples[i], x_samples[j])
+                term = div(num, den)
+                basis = mul(basis, term)
+        group = mul(y_samples[i], basis)
+        result = add(result, group)
+    return result
--- pyshamir/shamir.py
+++ pyshamir/shamir.py
@@ -1,24 +1,4 @@
-from ._utils import add, mul, div
-
-
-def interpolate_polynomial(x_samples, y_samples, x):
-    """
-    Takes N sample points and returns the value of the polynomial at x using Lagrange interpolation
-    """
-    limit = len(x_samples)
-    result = 0
-    for i in range(limit):
-        basis = 1
-        for j in range(limit):
-            if i != j:
-                num = add(x, x_samples[j])
-                den = add(x_samples[i], x_samples[j])
-                term = div(num, den)
-                basis = mul(basis, term)
-        group = mul(y_samples[i], basis)
-        result = add(result, group)
-    return result
-
+from ._utils import add, mul, div, interpolate_polynomial
 
 def combine(parts):
     # Verify enough parts are present
